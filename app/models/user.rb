8d3e687946085e6b50abf4acf1301f1c4dcb2829
mateo8a <mateo.ochoac@gmail.com>
2016-07-30 14:22:22 -0700

    Add test to check if first name and last name are not empty fields, and for profile name to be valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true, 
+                           uniqueness: true, 
+                           format: {
+                           	with: /a-zA-Z0-9_-/,
+                           	message: 'Format correctly please'
+                           }
+
   has_many :statuses
 
   def full_name
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,39 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "user should enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+
+  test "user should have a unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:mateo).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "user should have a profile name without spaces" do
+  	user = User.new
+  	user.profile_name = "this is not valid"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("Format correctly please")
+  end
+
 end
